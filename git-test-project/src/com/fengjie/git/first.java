afb3f0f47ec133fd4bfa86ac3b532bf7ba3f0be7
fengjie1997 <1499840292@qq.com>
2020-08-16 16:34:49 +0800

    clone-update

--- git-test-project/src/com/fengjie/git/first.java
+++ git-test-project/src/com/fengjie/git/first.java
@@ -3,6 +3,7 @@ package com.fengjie.git;
 public class first {
     public static void main(String[] args) {
         System.out.println('1');
+        System.out.println('2');
     }
     public void test() {
         int i = 10;
