85efeff0460e08c3fbb4198a064e5317f367645f
fengjie1997 <1499840292@qq.com>
2020-08-16 16:16:36 +0800

    第er次(其实是第three次提交)

--- git-test-project/src/com/fengjie/git/first.java
+++ git-test-project/src/com/fengjie/git/first.java
@@ -4,4 +4,9 @@ public class first {
     public static void main(String[] args) {
         System.out.println('1');
     }
+    public void test() {
+        int i = 10;
+        i += 10;
+        System.out.println(i);
+    }
 }
